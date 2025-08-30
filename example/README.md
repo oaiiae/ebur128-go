f4c08159690c34c774077ca5502cc62e37629d8b
Romain Libaert <git@rlibaert.dev>
2025-08-30 18:24:05 +0200

    feat: add example decoding with ffmpeg

---
+++ example/README.md
@@ -0,0 +1,1 @@
+`ffmpeg -loglevel quiet -i <your file> -ac 2 -ar 44100 -f f64le - | ./exe -`
---
+++ example/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+	"unsafe"
+
+	"github.com/oaiiae/ebur128-go"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: ", os.Args[0], "FILENAME...")
+		return
+	}
+
+	var r io.ReadCloser
+	switch os.Args[1] {
+	case "-":
+		r = os.Stdin
+	default:
+		var err error
+		r, err = os.Open(os.Args[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	defer r.Close()
+
+	const channels = 2
+	const sampling = 44100
+
+	st, err := ebur128.Init(channels, sampling, ebur128.ModeM)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer st.Destroy()
+
+	go func() {
+		for {
+			time.Sleep(400 * time.Millisecond)
+			fmt.Print("\r")
+			fmt.Print(st.LoudnessMomentary())
+		}
+	}()
+
+	src := make([]float64, channels)
+	buf := unsafe.Slice((*byte)(unsafe.Pointer(unsafe.SliceData(src))), 8*len(src))
+	for {
+		_, err := io.ReadFull(r, buf)
+		switch err {
+		case io.EOF:
+			return
+		case nil:
+		default:
+			fmt.Println(err)
+			return
+		}
+		st.AddFramesDouble(src, len(src)/channels)
+	}
+}
